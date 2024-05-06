0257ad7aacd23e96a548da4326e400f8ae9b72c9
DMarquezM <dmarquezmontano@nevada.unr.edu>
2024-05-06 20:25:21 +0000

    asd

---
+++ images.c
@@ -0,0 +1,87 @@
+//Erinstagram//
+//Authors: Dannie Marquez-Montano, Soycott A Razzak//
+//Date: 5/6/24//
+//Purpose: Create a mock image editing software.//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+char 
+int
+
+void save
+void dim
+void brighten
+void crop
+
+int main(){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
